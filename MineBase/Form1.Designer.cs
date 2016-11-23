65809f9867ad794121ae67a4247e82f31ca296b2
Сергей <@EVILIM>
2016-11-23 15:52:23 +0300

    Добавьте файлы проекта.

---
+++ MineBase/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace MineBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.infoMine = new MetroFramework.Controls.MetroGrid();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            ((System.ComponentModel.ISupportInitialize)(this.infoMine)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // infoMine
+            // 
+            this.infoMine.AllowUserToResizeRows = false;
+            this.infoMine.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.infoMine.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.infoMine.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.infoMine.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(119)))), ((int)(((byte)(53)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(133)))), ((int)(((byte)(72)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.infoMine.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.infoMine.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(133)))), ((int)(((byte)(72)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.infoMine.DefaultCellStyle = dataGridViewCellStyle2;
+            this.infoMine.Dock = System.Windows.Forms.DockStyle.Top;
+            this.infoMine.EnableHeadersVisualStyles = false;
+            this.infoMine.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.infoMine.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.infoMine.Location = new System.Drawing.Point(0, 0);
+            this.infoMine.Name = "infoMine";
+            this.infoMine.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(119)))), ((int)(((byte)(53)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(133)))), ((int)(((byte)(72)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.infoMine.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.infoMine.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.infoMine.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.infoMine.Size = new System.Drawing.Size(346, 187);
+            this.infoMine.Style = MetroFramework.MetroColorStyle.Orange;
+            this.infoMine.TabIndex = 0;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(0, 0);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.UseSelectable = true;
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.metroTile1.Location = new System.Drawing.Point(0, 194);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(346, 41);
+            this.metroTile1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroTile1.TabIndex = 1;
+            this.metroTile1.Text = "Сохранить изменения";
+            this.metroTile1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.Click += new System.EventHandler(this.metroTile1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(346, 235);
+            this.Controls.Add(this.metroTile1);
+            this.Controls.Add(this.infoMine);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "БД ";
+            ((System.ComponentModel.ISupportInitialize)(this.infoMine)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroGrid infoMine;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroTile metroTile1;
+    }
+}
+
---
+++ MineBase/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineBase
+{
+    public partial class Form1 : Form
+    {
+               private SqlConnection conn;
+        private DataSet dataS;
+        private SqlDataAdapter adap;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Desktop\интерес\MineBase\MineBase\dbmine.mdf;Integrated Security=True");
+            adap = new SqlDataAdapter();
+            dataS = new DataSet();
+            adap.SelectCommand = new SqlCommand("Select * from MineDB", conn);
+
+            adap.InsertCommand = new SqlCommand("Insert into MineDB (Id, Password, NamePC) values (@Id, @Pass, @name)", conn);
+            adap.InsertCommand.Parameters.Add("@Id", SqlDbType.NChar, 20, "Id");
+            adap.InsertCommand.Parameters.Add("@Pass", SqlDbType.Int, 20, "Password");
+            adap.InsertCommand.Parameters.Add("@name", SqlDbType.Char, 20, "NamePC");
+
+            adap.UpdateCommand = new SqlCommand("Update MineDB (Id, Password, NamePC) values (@Id, @Pass, @name)", conn);
+            adap.UpdateCommand.Parameters.Add("@Id", SqlDbType.NChar, 20, "Id");
+            adap.UpdateCommand.Parameters.Add("@Pass", SqlDbType.Int, 20, "Password");
+            adap.UpdateCommand.Parameters.Add("@name", SqlDbType.Char, 20, "NamePC");
+
+            adap.DeleteCommand = new SqlCommand("delete from MineDB where Id=@Id", conn);
+            adap.DeleteCommand.Parameters.Add("@Id", SqlDbType.Int, 10, "Id");
+
+            adap.Fill(dataS, "MineDB");
+            infoMine.DataSource = dataS.Tables["MineDB"];
+        }
+
+        private void metroTile1_Click(object sender, EventArgs e)
+        {
+            adap.Update(dataS.Tables["MineDB"]);
+        }
+    }
+}
